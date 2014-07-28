3846ad0b3e47748d59596efad068595a162f1eca
solodu <solo@cdc.im>
2014-07-28 17:36:23 +0800

    line endings

--- cft.js
+++ cft.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
 'use strict';
 var fs = require('fs-extra'),
-    stat = fs.stat,
-    path = require('path'),
-    exec = require('child_process').exec;
+	stat = fs.stat,
+	path = require('path'),
+	exec = require('child_process').exec;
 var src = path.join(__dirname, './' + process.argv.splice(2)[0])
 var dest = process.cwd();
 fs.mkdirs(src + '/js', function(err) {
-    if (err) return;
+	if (err) return;
 });
 fs.mkdirs(src + '/img', function(err) {
-    if (err) return;
+	if (err) return;
 });
 fs.mkdirs(src + '/css', function(err) {
-    if (err) return;
+	if (err) return;
 });
 fs.copy(src, dest, function(err) {
-    if (err) return console.error(err);
-    var install = exec('cd ' + dest + '/grunt && npm install',
-        function(error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
-    install.stdout.on('data', function(data) {
-        console.log(data);
-    });
-    install.stderr.on('data', function(data) {
-        console.log(data);
-    });
-    install.on('exit', function(code, signal) {
-        console.log(code);
-    });
+	if (err) return console.error(err);
+	var install = exec('cd ' + dest + '/grunt && npm install',
+		function(error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
+	install.stdout.on('data', function(data) {
+		console.log(data);
+	});
+	install.stderr.on('data', function(data) {
+		console.log(data);
+	});
+	install.on('exit', function(code, signal) {
+		console.log(code);
+	});
 });
