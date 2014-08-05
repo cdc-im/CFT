3dfd5baef2f30b15079cf76002ac6e56e62d02fe
solo <solo@cdc.im>
2014-08-05 17:45:15 +0800

    mkdirs callback

--- cft.js
+++ cft.js
@@ -8,30 +8,30 @@ var src = path.join(__dirname, './' + process.argv.splice(2)[0])
 var dest = process.cwd();
 fs.mkdirs(src + '/js', function(err) {
 	if (err) return;
-});
-fs.mkdirs(src + '/img', function(err) {
-	if (err) return;
-});
-fs.mkdirs(src + '/css', function(err) {
-	if (err) return;
-});
-fs.copy(src, dest, function(err) {
-	if (err) return console.error(err);
-	var install = exec('cd ' + dest + '/grunt && npm install',
-		function(error, stdout, stderr) {
-			console.log('stdout: ' + stdout);
-			console.log('stderr: ' + stderr);
-			if (error !== null) {
-				console.log('exec error: ' + error);
-			}
+	fs.mkdirs(src + '/img', function(err) {
+		if (err) return;
+		fs.mkdirs(src + '/css', function(err) {
+			if (err) return;
+			fs.copy(src, dest, function(err) {
+				if (err) return console.error(err);
+				var install = exec('cd ' + dest + '/grunt && npm install',
+					function(error, stdout, stderr) {
+						console.log('stdout: ' + stdout);
+						console.log('stderr: ' + stderr);
+						if (error !== null) {
+							console.log('exec error: ' + error);
+						}
+					});
+				install.stdout.on('data', function(data) {
+					console.log(data);
+				});
+				install.stderr.on('data', function(data) {
+					console.log(data);
+				});
+				install.on('exit', function(code, signal) {
+					console.log(code);
+				});
+			});
 		});
-	install.stdout.on('data', function(data) {
-		console.log(data);
-	});
-	install.stderr.on('data', function(data) {
-		console.log(data);
-	});
-	install.on('exit', function(code, signal) {
-		console.log(code);
 	});
 });
